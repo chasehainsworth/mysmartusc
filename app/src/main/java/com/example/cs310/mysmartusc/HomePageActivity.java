e97dc9bb4cc61ed1d62495dc6edc23aa537549ea
daltonb@usc.edu <Powermac8!>
2018-10-14 16:37:11 -0700

    Changed to Java.

---
+++ app/src/main/java/com/example/cs310/mysmartusc/HomePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs310.mysmartusc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomePageActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_activity);
+        TextView helloTextView = (TextView) findViewById(R.id.hello);
+
+    }
+}
