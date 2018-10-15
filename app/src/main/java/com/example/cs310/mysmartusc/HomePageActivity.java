bc1e8777758672cfb959abf82bfa2504e0012d54
daltonb@usc.edu <Powermac8!>
2018-10-14 17:04:36 -0700

    Link UI elements to code.

--- app/src/main/java/com/example/cs310/mysmartusc/HomePageActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/HomePageActivity.java
@@ -2,6 +2,7 @@ package com.example.cs310.mysmartusc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomePageActivity extends Activity {
@@ -9,7 +10,9 @@ public class HomePageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_activity);
 
-
+        TextView tv = (TextView) findViewById(R.id.homeLabel);
+        Button notifications = (Button) findViewById(R.id.notificationsButton);
+        Button settings = (Button) findViewById(R.id.settingsButton);
 
     }
 }
