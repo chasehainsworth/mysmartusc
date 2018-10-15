a402a94853c9608815bcd02c1a95f51380875ac4
daltonb@usc.edu <Powermac8!>
2018-10-14 17:07:35 -0700

    Added loginActivity

---
+++ app/src/main/java/com/example/cs310/mysmartusc/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.cs310.mysmartusc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_activity);
+    }
+}
