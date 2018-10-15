7f2f5cc44bb6e01cccb8b8ca1c9a7c2f05025951
daltonb@usc.edu <Powermac8!>
2018-10-14 23:46:14 -0700

    Added EmailViewerActivity

--- app/src/main/java/com/example/cs310/mysmartusc/HomePageActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/HomePageActivity.java
@@ -1,7 +1,9 @@
 package com.example.cs310.mysmartusc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,9 +12,33 @@ public class HomePageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_activity);
 
+        /**
+         * To pass data between activities:
+         * intent.putExtra(String key, Object data)
+         * intent.getStringExtra(String key)
+         */
+
         TextView tv = (TextView) findViewById(R.id.homeLabel);
         Button notifications = (Button) findViewById(R.id.notificationsButton);
         Button settings = (Button) findViewById(R.id.settingsButton);
+
+        notifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent notificationsIntent = new Intent(HomePageActivity.this, NotificationsActivity.class);
+                //startActivity(notificationsIntent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent settingsIntent = new Intent(HomePageActivity.this, SettingsActivity.class);
+                //startActivity(settingsIntent);
+            }
+        });
+
+
 
     }
 }
