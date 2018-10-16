63818cb00a42211255f550c94f4eb77b772f5001
alexyoung <alexangy@usc.edu>
2018-10-15 19:02:55 -0700

    going to pull new files. must commit fist

--- app/src/main/java/com/example/cs310/mysmartusc/NotificationActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/NotificationActivity.java
@@ -1,7 +1,9 @@
 package com.example.cs310.mysmartusc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,8 +16,32 @@ public class NotificationActivity extends Activity {
         Button saved = (Button) findViewById(R.id.submit);
         Button urgent = (Button) findViewById(R.id.urgent);
         Button spam = (Button) findViewById(R.id.spam);
+
+        /*
+        saved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, SavedPageActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+
+        urgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, UrgentPageActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+
+        spam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, SpamPageActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+*/
     }
-
-
 
 }
