dfb55ddeb58ac99202c08172758e4c6b713074ba
Anay Patel <anaypate@usc.edu>
2018-10-15 18:43:21 -0700

    created SpamActivity, UrgentActivity class/layout xml files, added to the AndroidManifest, updated NotificationActivity buttons

--- app/src/main/java/com/example/cs310/mysmartusc/NotificationActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/NotificationActivity.java
@@ -1,7 +1,9 @@
 package com.example.cs310.mysmartusc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,31 @@ public class NotificationActivity extends Activity {
         Button saved = (Button) findViewById(R.id.submit);
         Button urgent = (Button) findViewById(R.id.urgent);
         Button spam = (Button) findViewById(R.id.spam);
+
+        saved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, SaveActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+
+        urgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, UrgentActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+
+        spam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent notificationsIntent = new Intent(NotificationActivity.this, SpamActivity.class);
+                startActivity(notificationsIntent);
+            }
+        });
+
     }
 
 
