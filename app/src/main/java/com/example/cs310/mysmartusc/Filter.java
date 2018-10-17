04b3ee1f090f4231faa5580826ac1538478fc2fd
daltonb@usc.edu <Powermac8!>
2018-10-16 18:16:00 -0700

    Filter done!

--- app/src/main/java/com/example/cs310/mysmartusc/Filter.java
+++ app/src/main/java/com/example/cs310/mysmartusc/Filter.java
@@ -8,6 +8,9 @@ public class Filter {
     private List<String> bodyKeywords;
     private List<String> subjectKeywords;
 
+    //What needs to be the match ratio to return true?
+    private double keywordMatchFrequency = 0.1;
+
     public Filter(){
         this.emailAddresses = new ArrayList<>();
         this.bodyKeywords = new ArrayList<>();
@@ -39,6 +42,34 @@ public class Filter {
 
     public boolean sort(Email email){
         //Determine what category the email belongs to.
+
+        String currentEmailSubject = email.getSubject();
+        String currentEmailSender = email.getSender();
+        String currentEmailBody = email.getBody();
+
+        if(emailAddresses.contains(currentEmailSender)){
+            return true;
+        } else if (subjectKeywords.contains(currentEmailSubject)){
+            return true;
+        } else {
+            //Keywords may still be in the body we need to tokenize...
+            String words[] = currentEmailBody.split(" ");
+            double wordsLength = words.length;
+
+            //Figuring out how many matches there are.
+            int matches = 0;
+            for (String word : words){
+                if (bodyKeywords.contains(word)){
+                    matches++;
+                }
+            }
+
+            //Finding the frequency of the matched keywords to all the
+            //words in the email.
+            if(wordsLength / ((double) bodyKeywords.size()) >= 0.1){
+                return true;
+            }
+        }
 
         return false;
     }
