be99c9073e250b666b2bd264810013fc8e5ccdad
daltonb@usc.edu <Powermac8!>
2018-10-16 17:40:56 -0700

    Initial structure of Filter.java

--- app/src/main/java/com/example/cs310/mysmartusc/Filter.java
+++ app/src/main/java/com/example/cs310/mysmartusc/Filter.java
@@ -1,5 +1,19 @@
 package com.example.cs310.mysmartusc;
 
-public class Filter {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Filter {
+    private List<String> emailAddresses;
+    private List<String> bodyKeywords;
+    private List<String> subjectKeywords;
+
+    public Filter(){
+        this.emailAddresses = new ArrayList<>();
+        this.bodyKeywords = new ArrayList<>();
+        this.subjectKeywords = new ArrayList<>();
+    }
+    public void addEmailAddress(String emailAddress){
+
+    }
 }
