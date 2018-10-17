3772a81d98b7e91bdf61714ee3dfa7403aeeb0a6
daltonb@usc.edu <Powermac8!>
2018-10-16 17:56:49 -0700

    Added Email class

---
+++ app/src/main/java/com/example/cs310/mysmartusc/Email.java
@@ -0,0 +1,17 @@
+package com.example.cs310.mysmartusc;
+
+public class Email {
+    private String subject, body, sender;
+
+    public Email(){
+        this.subject = "";
+        this.body = "";
+        this.sender = "";
+    }
+
+    public Email(String subject, String body, String sender){
+        this.subject = subject;
+        this.sender = sender;
+        this.body = body;
+    }
+}
--- app/src/main/java/com/example/cs310/mysmartusc/Filter.java
+++ app/src/main/java/com/example/cs310/mysmartusc/Filter.java
@@ -14,6 +14,31 @@ public class Filter {
         this.subjectKeywords = new ArrayList<>();
     }
     public void addEmailAddress(String emailAddress){
+        this.emailAddresses.add(emailAddress);
+    }
+
+    public void addBodyKeyword(String keyword){
+        this.bodyKeywords.add(keyword);
+    }
+
+    public void addSubjectKeyword(String keyword){
+        this.addSubjectKeyword(keyword);
+    }
+
+    public List<String> getEmailAddresses(){
+        return this.emailAddresses;
+    }
+
+    public List<String> getBodyKeywords(){
+        return this.bodyKeywords;
+    }
+
+    public List<String> getSubjectKeywords(){
+        return this.subjectKeywords;
+    }
+
+    public boolean sort(Email email){
+        //Determine what category the email belongs to.
 
     }
 }
