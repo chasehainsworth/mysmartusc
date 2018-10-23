ed5f22f29e79e2952b50cf3506a4ba588deb3d78
Chase Hainsworth <chasehainsworth@gmail.com>
2018-10-22 22:32:57 -0700

    Spam onStart()

--- app/src/main/java/com/example/cs310/mysmartusc/SaveActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/SaveActivity.java
@@ -4,64 +4,72 @@ import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.database.Cursor;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
-import android.database.Cursor;
 
-public class SaveActivity extends Activity {
+public class SpamActivity extends Activity {
 
     private ArrayList<Email> emails;
     DatabaseInterface db;
+    String mUsername;
+    String mType;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.save_activity);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        refreshView();
+    }
 
-        emails = new ArrayList<>();
-        db = DatabaseInterface.getInstance(this);
+    private void refreshView() {
+        emails.clear();
+        Log.e("SpamActivity", "Getting " + mUsername + ", type: " + mType);
+        Cursor cursor = db.getEmailByType(mUsername, mType);
 
-        Account account = (Account)getIntent().getParcelableExtra("account");
-        String user = account.name;
-        String type = "saved";
-
-        Cursor cursor = db.getEmailByType(user, type);
-
-        if (cursor != null && cursor.getCount() > 0 ) {
-            if  (cursor.moveToFirst()) {
+        if (cursor != null && cursor.getCount() > 0) {
+            if (cursor.moveToFirst()) {
                 do {
                     String subject = cursor.getString(cursor.getColumnIndex("SUBJECT"));
                     String body = cursor.getString(cursor.getColumnIndex("BODY"));
                     String sender_user = cursor.getString(cursor.getColumnIndex("SENDER_USER"));
-                    String sender_domain = cursor.getString(cursor.getColumnIndex("SENDER_USER"));
+                    String sender_domain = cursor.getString(cursor.getColumnIndex("SENDER_DOMAIN"));
 
+
+                    Log.e("SpamActivity", "Creating email with subject: " + subject);
                     emails.add(new Email(subject, body, sender_user + "@" + sender_domain));
-                }while (cursor.moveToNext());
+
+
+                } while (cursor.moveToNext());
+            }
+        } else {
+            if (cursor == null) {
+                Log.e("SpamActivity", "The cursor to get the emails is null!");
+            } else {
+                Log.e("SpamActivity", "There are no emails here!");
             }
         }
         cursor.close();
 
         ArrayList<String> emailHeaders = new ArrayList<>();
 
-        for(Email e : emails){
+        for (Email e : emails) {
             emailHeaders.add(e.getSubject());
         }
 
         ListView listView = (ListView) findViewById(R.id.listView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, emailHeaders);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent emailIntent = new Intent(SaveActivity.this, EmailViewerActivity.class);
+                Intent emailIntent = new Intent(SpamActivity.this, EmailViewerActivity.class);
 
                 emailIntent.putExtra("subject", emails.get(position).getSubject());
                 emailIntent.putExtra("body", emails.get(position).getBody());
@@ -71,6 +79,19 @@ public class SaveActivity extends Activity {
             }
 
         });
+    }
+
+    protected void onCreate(Bundle urgentInstanceState) {
+        super.onCreate(urgentInstanceState);
+        setContentView(R.layout.save_activity);
+
+        emails = new ArrayList<>();
+        db = DatabaseInterface.getInstance(this);
+        Account account = (Account) getIntent().getParcelableExtra("account");
+        mUsername = account.name;
+        mType = "spam";
+        refreshView();
+
 
     }
 
