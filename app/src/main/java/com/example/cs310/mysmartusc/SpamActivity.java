d87fbb3e46a706c7fd33c5d2d45ed09ebd0431c8
daltonb@usc.edu <Powermac8!>
2018-10-22 22:31:39 -0700

    spamactivity done

--- app/src/main/java/com/example/cs310/mysmartusc/SpamActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/SpamActivity.java
@@ -4,6 +4,7 @@ import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,44 +19,54 @@ public class SpamActivity extends Activity {
 
     private ArrayList<Email> emails;
     DatabaseInterface db;
+    String mUsername;
+    String mType;
 
-    protected void onCreate(Bundle spamInstanceState) {
-        super.onCreate(spamInstanceState);
-        setContentView(R.layout.spam_activity);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        refreshView();
+    }
 
+    private void refreshView() {
+        emails.clear();
+        Log.e("SpamActivity", "Getting " + mUsername + ", type: " + mType);
+        Cursor cursor = db.getEmailByType(mUsername, mType);
 
-        emails = new ArrayList<>();
-        db = DatabaseInterface.getInstance(this);
-        Account account = (Account)getIntent().getParcelableExtra("account");
-        String user = account.name;
-        String type = "spam";
-
-        Cursor cursor = db.getEmailByType(user, type);
-
-        if (cursor != null && cursor.getCount() > 0 ) {
-            if  (cursor.moveToFirst()) {
+        if (cursor != null && cursor.getCount() > 0) {
+            if (cursor.moveToFirst()) {
                 do {
                     String subject = cursor.getString(cursor.getColumnIndex("SUBJECT"));
                     String body = cursor.getString(cursor.getColumnIndex("BODY"));
                     String sender_user = cursor.getString(cursor.getColumnIndex("SENDER_USER"));
-                    String sender_domain = cursor.getString(cursor.getColumnIndex("SENDER_USER"));
+                    String sender_domain = cursor.getString(cursor.getColumnIndex("SENDER_DOMAIN"));
 
+
+                    Log.e("SpamActivity", "Creating email with subject: " + subject);
                     emails.add(new Email(subject, body, sender_user + "@" + sender_domain));
-                }while (cursor.moveToNext());
+
+
+                } while (cursor.moveToNext());
+            }
+        } else {
+            if (cursor == null) {
+                Log.e("SpamActivity", "The cursor to get the emails is null!");
+            } else {
+                Log.e("SpamActivity", "There are no emails here!");
             }
         }
         cursor.close();
 
         ArrayList<String> emailHeaders = new ArrayList<>();
 
-        for(Email e : emails){
+        for (Email e : emails) {
             emailHeaders.add(e.getSubject());
         }
 
         ListView listView = (ListView) findViewById(R.id.listView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, emailHeaders);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -69,6 +80,19 @@ public class SpamActivity extends Activity {
             }
 
         });
+    }
+
+    protected void onCreate(Bundle spamInstanceState) {
+        super.onCreate(spamInstanceState);
+        setContentView(R.layout.save_activity);
+
+        emails = new ArrayList<>();
+        db = DatabaseInterface.getInstance(this);
+        Account account = (Account) getIntent().getParcelableExtra("account");
+        mUsername = account.name;
+        mType = "spam";
+        refreshView();
+
 
     }
 
