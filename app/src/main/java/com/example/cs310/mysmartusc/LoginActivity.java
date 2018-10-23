52d5611033a37467e4d01664b6846c065d1afd02
Chase Hainsworth <chasehainsworth@gmail.com>
2018-10-22 19:15:38 -0700

    Delete unnecessary imports and code from LoginActivity

--- app/src/main/java/com/example/cs310/mysmartusc/LoginActivity.java
+++ app/src/main/java/com/example/cs310/mysmartusc/LoginActivity.java
@@ -4,25 +4,17 @@ import android.accounts.Account;
 import android.app.ActivityManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.ProgressDialog;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -33,31 +25,7 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.ListMessagesResponse;
-import com.google.api.services.gmail.model.Message;
-import com.google.api.services.gmail.model.MessagePartHeader;
 
-import java.io.IOException;
-import java.lang.ref.WeakReference;
-import java.util.Collections;
-import java.util.List;
-
-/**
- * Activity to demonstrate using the Google Sign In API with a Google API that uses the Google
- * Java Client Library rather than a Google Play services API.
- * for how to access the People API using this method.
- *
- * In order to use this Activity you must enable the People API on your project. Visit the following
- * link and replace 'YOUR_PROJECT_ID' to enable the API:
- * https://console.developers.google.com/apis/api/people.googleapis.com/overview?project=YOUR_PROJECT_ID
- */
 public class LoginActivity extends AppCompatActivity implements
         View.OnClickListener {
 
@@ -71,19 +39,10 @@ public class LoginActivity extends AppCompatActivity implements
 
     // Request codes
     private static final int RC_SIGN_IN = 9001;
-    private static final int RC_RECOVERABLE = 9002;
-
-    // Global instance of the HTTP transport
-    private static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
-
-    // Global instance of the JSON factory
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     private GoogleSignInClient mGoogleSignInClient;
 
-    private GmailWrapperService mBoundService;
     private Account mAccount;
-    private Gmail mService;
     private ProgressDialog mProgressDialog;
 
     @Override
@@ -100,8 +59,7 @@ public class LoginActivity extends AppCompatActivity implements
             mAccount = savedInstanceState.getParcelable(KEY_ACCOUNT);
         }
 
-        // Configure sign-in to request the user's ID, email address, basic profile,
-        // and readonly access to contacts.
+        // Configure sign-in to request the user's email address and emails.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestScopes(new Scope(EMAIL_SCOPE))
                 .requestEmail()
@@ -109,9 +67,6 @@ public class LoginActivity extends AppCompatActivity implements
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        // Show a standard Google Sign In button. If your application does not rely on Google Sign
-        // In for authentication you could replace this with a "Get Google Contacts" button
-        // or similar.
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
@@ -165,14 +120,6 @@ public class LoginActivity extends AppCompatActivity implements
             handleSignInResult(task);
         }
 
-        // Handling a user-recoverable auth exception
-//        if (requestCode == RC_RECOVERABLE) {
-//            if (resultCode == RESULT_OK) {
-//                getContacts();
-//            } else {
-//                Toast.makeText(this, R.string.msg_contacts_failed, Toast.LENGTH_SHORT).show();
-//            }
-//        }
     }
 
     private void continueToHomepage()
@@ -189,9 +136,7 @@ public class LoginActivity extends AppCompatActivity implements
     }
 
     private void signOut() {
-        // Signing out clears the current authentication state and resets the default user,
-        // this should be used to "switch users" without fully un-linking the user's google
-        // account from your application.
+        // Signing out clears the current authentication state
         mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -218,25 +163,6 @@ public class LoginActivity extends AppCompatActivity implements
             //Add the user to the database
             DatabaseInterface db = DatabaseInterface.getInstance(this);
             db.addUser(account.getEmail());
-
-//            ServiceConnection connection = new ServiceConnection() {
-//                @Override
-//                public void onServiceConnected(ComponentName name, IBinder service) {
-//                    mBoundService = ((GmailWrapperService.LocalBinder)service).getService();
-//                    Intent saveIntent = new Intent(LoginActivity.this, HomePageActivity.class);
-//                    saveIntent.putExtra("account", mAccount);
-//                    saveIntent.putExtra("service", mBoundService);
-//                    startActivity(saveIntent);
-//                }
-//
-//                @Override
-//                public void onServiceDisconnected(ComponentName name) {
-//                    mBoundService = null;
-//                }
-//            };
-//            bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
-//            while(mBoundService == null) {}
-//            mBoundService.reloadKeywords();
         } catch (ApiException e) {
             Log.w(TAG, "handleSignInResult:error", e);
             // Clear the local account
@@ -304,8 +230,7 @@ public class LoginActivity extends AppCompatActivity implements
     }
 
     private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
+        // Create the NotificationChannel, but only on API 26+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
